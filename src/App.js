8fe4c1bcb273d91fd38d30eff2c9ef60d02aaf79
RustyPPsH <rafaelsonoki@hotmail.com>
2023-05-17 20:06:58 -0300

    gpt pls

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import api from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* 1. Criaremos uma classe para nossa aplicação e
+damos o extends em Component. Observe que agora
+é adicionado no import. */
+class App extends Component {
+
+  // 3. Criamos um state para armazenar os filmes
+  state = {
+    filmes: [],
+  }
+
+  /* 4. Vamos adicionar um componente para buscar
+  os dados na API. */
+  async componentDidMount() {
+    /* 5. Veja que a constante response utiliza o await
+    porque usamos async. */
+    const response = await api.get('');
+
+    /* Colocamos o console log para poder inspecionar
+    os elementos para debug, mas não é utilizado na 
+    aplicação*/
+    //console.log(response.data);
+
+    // 6. jovar os valores recebidos para filmes
+    this.setState({ filmes: response.data });
+  }
+
+  // 2. Criamos nosso render
+  render() {
+    /* 7. Vamos buscar os dados de this.state e jogar para
+    a posição filmes*/
+    const { filmes } = this.state;
+
+    return (
+      <div>
+        <h1>Filmes</h1>
+        { console.log(filmes)}
+
+        {/* 8. Como temos uma array, então vamos usar um maps
+        para percorrê-la. Vejaj que temos que ver no resultado
+        da API os atributos que vamos mostrar, como name e url.*/}
+        {filmes.map(filme => (
+          <li key={filme.show.id}>
+            <h2>
+              {filme.show.name}
+            </h2>
+            <p><a href={filme.show.url} target="_blank">
+              {filme.show.url}</a></p>
+          </li>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
